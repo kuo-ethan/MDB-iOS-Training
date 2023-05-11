68e3764f6782ce1f0a3022a879f2f0e2bd2dea18
Ethan Kuo <ek0335@berkeley.edu>
2023-05-10 21:42:13 -0700

    ignore

--- MDB Social/MDB Social/Extensions/UIColor+ColorScheme.swift
+++ MDB Social/MDB Social/Extensions/UIColor+ColorScheme.swift
@@ -9,13 +9,18 @@ import UIKit
 
 extension UIColor {
     
-    public static let primary = UIColor(hex: "#1da1f2")!
+    // public static let primary = UIColor(hex: "#1da1f2")!
+    public static let primary = UIColor(hex: "#ff5552")!
     
     public static let primaryText = UIColor(hex: "#303545")!
     
     public static let secondaryText = UIColor(hex: "#5d6165")!
     
     public static let tertiaryText = UIColor(hex: "#82878f")!
+    
+    public static let taskEventColor = UIColor(hex: "#e0e0e0")!
+    
+    public static let commitmentEventColor = UIColor(hex: "#ff5552")!
     
     public static let background = UIColor.white
 }
